21116fbf88e17e530d95038e848f4b93a4365ae0
= <ss722980@gmail.com>
2021-06-21 12:32:04 +0530

    Changed hk21.settings.py

--- hk21/settings.py
+++ hk21/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,8 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+os.path.join(BASE_DIR / "static"),
 ]
 
 # Default primary key field type
